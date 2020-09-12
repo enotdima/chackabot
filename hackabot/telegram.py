ed1788087ae67450b251ca9470e2e0c51da24d68
VyatkinAlexey <vyatkin.ad@phystech.edu>
2020-09-13 02:55:28 +0300

    ready to implement quizzes

--- hackabot/telegram.py
+++ hackabot/telegram.py
@@ -63,7 +63,6 @@ def run_bot(config_path: str):
     def _start(message: telebot.types.Message):
         with locks[message.chat.id]:
             guy = message.from_user.id
-            print(guy)
             curr_case[guy] = -1
             money[guy] = 1000
             exp[guy] = 0
@@ -99,74 +98,82 @@ def run_bot(config_path: str):
             keyboard = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
             answers = []
             for quiz_key in quiz_text['quiz_answers']:
-                answers.append(types.KeyboardButton(text=quiz_text['quiz_answers'][quiz_key]['button_text']))
+                if quiz_key == 'case4': # banana mode
+                    answers.append(types.KeyboardButton(text=quiz_text['quiz_answers'][quiz_key]['button_text']))
+                                                        #request_location=True))
+                else:
+                    answers.append(types.KeyboardButton(text=quiz_text['quiz_answers'][quiz_key]['button_text']))
             keyboard.add(*answers)
 
             _send(message, response, keyboard)
-            
 
-    @bot.message_handler(commands=['FindMerchant'])
-    def _find_merchant(message: telebot.types.Message):
-        print("MERCHANT TEXT")
-        print(message.text)
-        item_name = message.text.replace('/FindMerchant', '').strip()
-        if len(item_name) == 0:
-            item_name = 'хлеб'
-        else:
-            item_name = item_name.split()[0]
-        with locks[message.chat.id]:
-            response_prefix = f'Вот где вы можете купить {item_name}' + '\n'
+    def _find_product(item_name: str):
 
-            site_path = 'https://api-common-gw.tinkoff.ru/search/api/v1/search_merchants'
-            header_content = "'Content-Type: application/json'"
-            # TODO here should be user geoposition
-            data_raw_dict = {
-                'geo_query':
-                    {
-                        'bottom_right': {
-                            'lat': 55.73741399385868,
-                            'lon': 37.56961595778349
-                        },
-                        'top_left': {
-                            "lat": 55.742244061297384,
-                            "lon": 37.56546389822844
-                        }
+        response_prefix = f'Вот где вы можете купить {item_name}' + '\n'
+
+        site_path = 'https://api-common-gw.tinkoff.ru/search/api/v1/search_merchants'
+        header_content = "'Content-Type: application/json'"
+        # TODO here should be user geoposition
+        data_raw_dict = {
+            'geo_query':
+                {
+                    'bottom_right': {
+                        'lat': 55.73741399385868,
+                        'lon': 37.56961595778349
                     },
-                'query': item_name,
-                'count': 5
-            }
+                    'top_left': {
+                        "lat": 55.742244061297384,
+                        "lon": 37.56546389822844
+                    }
+                },
+            'query': item_name,
+            'count': 5
+        }
 
-            query = f"curl --location --request POST '{site_path}' " + \
-                    f"--header {header_content} " + \
-                    f"--data-raw '{json.dumps(data_raw_dict, indent=2, ensure_ascii=False)}'"
+        query = f"curl --location --request POST '{site_path}' " + \
+                f"--header {header_content} " + \
+                f"--data-raw '{json.dumps(data_raw_dict, indent=2, ensure_ascii=False)}'"
+        print('QUERY:')
+        print(query)
+        proc = subprocess.Popen(query, stdout=subprocess.PIPE, shell=True)
+        (out, err) = proc.communicate()
 
-            proc = subprocess.Popen(query, stdout=subprocess.PIPE, shell=True)
-            (out, err) = proc.communicate()
+        response_suffix = ''
+        out = out.decode('utf-8')
+        out = out.replace('null', '""')
+        out = out.replace('true', 'True')
+        out = out.replace('false', 'False')
+        out = eval(out)
+        for ix, item in enumerate(out['search_result']['hits']):
+            response_suffix += '\t'.join([str(ix + 1), item['mcc'][0] + ':', item['address']])
+            response_suffix += '\n'
 
-            response_suffix = ''
-            out = out.decode('utf-8')
-            out = out.replace('null', '""')
-            out = out.replace('true', 'True')
-            out = out.replace('false', 'False')
-            out = eval(out)
-            for ix, item in enumerate(out['search_result']['hits']):
-                response_suffix += '\t'.join([str(ix + 1), item['mcc'][0] + ':', item['address']])
-                response_suffix += '\n'
+        response = f'{response_prefix}{response_suffix}'
 
-            response = f'{response_prefix}{response_suffix}'
-
-            _send(message, response)
+        return response
 
     def _send_response(message: telebot.types.Message):
         print(f'current message: {message}')
-        print(curr_case)
+        print('#### LOCATION')
+        print(message.location)
+        print('########')
+
         response = None
         chat_id = message.chat.id
         user_id = message.from_user.id if message.from_user else '<unknown>'
-        print(user_id)
+
         keyboard = None
         with locks[chat_id]:
             try:
+                # quiz part
+                # TODO add case and keyboard logic as in quest
+                for quiz_key in quiz_text['quiz_answers']:
+                    if message.text == quiz_text['quiz_answers'][quiz_key]['button_text']:
+                        # TODO
+                        # for quiz_key == "case4" add to normal response:
+                        # _find_product("банан")
+                        pass
+
                 case = None
                 prev_node = curr_case[user_id]
                 if prev_node == -1:
@@ -190,7 +197,7 @@ def run_bot(config_path: str):
                             response = None
                 keyboard = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
                 answers = []
-                print(curr_case[user_id], type(curr_case[user_id]))
+                print('curr_case[user_id]:', curr_case[user_id])
                 if curr_case[user_id] > -1:
                     for i in button_texts['answers_' + str(curr_case[user_id])].keys():
                         answers.append(types.KeyboardButton(text = button_texts['answers_'+ str(curr_case[user_id])][i]['button_text']))
